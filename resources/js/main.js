61e466fd67f06e67708ffcf9e2336f5eb7717d76
mattlawiscool <mattlawiscool@gmail.com>
2017-08-19 13:18:25 -0700

    Cairn Mock Up
    Landing page proposal

---
+++ resources/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.carousel').slick({
+        autoplay: true,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        fade: true,
+        cssEase: 'linear',
+        speed: 500,
+
+    });
+});
